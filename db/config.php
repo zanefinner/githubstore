1e052ecfade0ffee074052087ec567829ea64e0d
Zane Finner <zanefinnergm@gmail.com>
2021-06-07 16:39:36 -0400

    connect mysql

---
+++ db/config.php
@@ -0,0 +1,5 @@
+<?php
+    $host = 'localhost';
+    $dbname = 'githubstore';
+    $username = 'zane';
+    $password = '5245';
---
+++ index.php
@@ -0,0 +1,2 @@
+<?php echo "php working"?>
+<?php require_once("db/connect.php");

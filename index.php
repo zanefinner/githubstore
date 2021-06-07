d90dd7fb2c10b8341d45193300563edb26229598
Zane Finner <zanefinnergm@gmail.com>
2021-06-07 16:46:16 -0400

    start homepage draft

--- index.php
+++ index.php
@@ -1,2 +1,29 @@
-<?php echo "php working"?>
-<?php require_once("db/connect.php");
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesheets/common.css" />
+    <title>Github Store</title>
+</head>
+
+<body>
+    <div class="navbar">
+        <form action="search.php" method="GET">
+            <input id="search" name="search" type="text" placeholder="Type here">
+            <input id="submit" type="submit" value="Search">
+        </form>
+
+        <a href="..">Login</a>
+        <a href="..">Sign Up</a>
+    </div>
+    <div class="padding"></div>
+    <div class="container">
+        <h2>New Releases</h2>
+        <div class="product-slider"></div>
+    </div>
+</body>
+
+</html>

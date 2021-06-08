f359ff51e81d02cfa485eee1d923e027206814cf
Zane Finner <zanefinnergm@gmail.com>
2021-06-08 01:38:21 -0400

    materialize library

--- index.php
+++ index.php
@@ -6,24 +6,96 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="stylesheets/common.css" />
+    <link rel="stylesheet" href="stylesheets/materialize.css" />
+
     <title>Github Store</title>
 </head>
 
 <body>
-    <div class="navbar">
-        <form action="search.php" method="GET">
-            <input id="search" name="search" type="text" placeholder="Type here">
-            <input id="submit" type="submit" value="Search">
-        </form>
-
-        <a href="..">Login</a>
-        <a href="..">Sign Up</a>
+    <nav>
+        <div class="nav-wrapper">
+            <a href="#" class="brand-logo center">Github Store</a>
+            <ul id="nav-mobile" class="left hide-on-med-and-down">
+                <li><a href="sass.html">For You</a></li>
+                <li><a href="badges.html">Gaming</a></li>
+                <li><a href="collapsible.html">Entertainment</a></li>
+                <li><a href="collapsible.html">Development</a></li>
+            </ul>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="sass.html">Account</a></li>
+            </ul>
+        </div>
+    </nav>
+    <div class="banner-horizontal">
+        <h1 class="center-align">For You</h1>
     </div>
-    <div class="padding"></div>
+    <div class="spacer-30px"></div>
     <div class="container">
-        <h2>New Releases</h2>
-        <div class="product-slider"></div>
+        <div class="row">
+            <div class="col s6 m4 l4 xl4 ">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="images/sample-1.jpg">
+                        <span class="card-title">Card Title</span>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">This is a link</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col s6 m4 l4 xl4 ">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="images/sample-1.jpg">
+                        <span class="card-title">Card Title</span>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">This is a link</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col s6 m4 l4 xl4 ">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="images/sample-1.jpg">
+                        <span class="card-title">Card Title</span>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">This is a link</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col s6 m4 l4 xl4 ">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="images/sample-1.jpg">
+                        <span class="card-title">Card Title</span>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">This is a link</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </div>
+
 </body>
 
 </html>
---
+++ services/crawl_github.php
@@ -0,0 +1,3 @@
+<?php 
+    //Crawl github, return data
+?>
